06ece1647daa983cc94ab902aa5a7387e0136e23
hvferreira <heldervferreira@gmail.com>
2023-02-25 15:24:57 +0000

    Row Create

---
+++ src/main/java/org/example/Seat.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Seat {
+
+    private final int ColumnStart = 1;
+    private final int ColumnEnd = 5;
+    private Row row;
+    private String seatNumb;
+
+    public Seat(){
+
+    }
+
+
+}
